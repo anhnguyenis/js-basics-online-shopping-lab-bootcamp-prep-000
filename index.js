29785a53e668d45fd00bd8cb553a9969ca2d8589
Anh Nguyen <anhnguyenis@gmail.com>
2020-01-05 18:05:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,23 +17,7 @@ function addToCart(itemName) {
 }
 
 function viewCart() {
-   if (cart.length === 0) {
-    console.log(`Your shopping cart is empty.`);
-  } else {
-    var statment = "In your cart, you have ";
-      for (var i = 0; i < cart.length; i++) {
-          var currentItem = cart[i];
-          
-          for (var itemName in currentItem) {
-            statment += `In your cart, you have ${itemName} at ${currentItem[itemName]},`;
-            
-            if (i === cart.length -1) {
-              statment += `and ${itemName} at ${currentItem[itemName]}.`;
-            }
-          }
-      }
-    console.log(statement);
-  }
+  // write your code here
 }
 
 function total() {
